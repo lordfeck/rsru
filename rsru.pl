26e9050ac0ba41e711b360a60e7b55830623ebe3
lordfeck <63065906+lordfeck@users.noreply.github.com>
2020-10-31 19:31:39 +0000

    Issue #8: Make site details in HTML template customisable The site name, HTML TITLE, description can now all be customised. Thus this feature is complete.
    Issue #2 Support generation of category pages
    Some preliminary work on this feature. We now have a separate HTML file
    with the template for each "tab", which will then be filled in.
    We've yet to implement a solution for the IS_ACTIVE tab,
    but the links appear.

--- rsru.pl
+++ rsru.pl
@@ -16,13 +16,16 @@ use v5.10;
 #===============================================================================
 # Begin user-configurable
 #===============================================================================
-my $tplinc = "./html";      # Where shall I find my easel?
-my $entrydir = "./entries"; # Where shall I find my inspiration?
-my $out = "./rsru";         # Where shall I place my finished work?
-my $tpl = "$tplinc/rsru_template.html"; # What is my easel named?
+my $tplinc = "./html";
+my $entrydir = "./entries";
+my $out = "./rsru";
+my $tpl = "$tplinc/rsru_template.html";
 my $blankentry = "$tplinc/rsru_entry.html";
+my $blankCatBar = "$tplinc/rsru_cat.html";
 my $fnPre = "rsru";
 my $siteName = "RSRU";
+my $siteHeaderDesc = "Really Small, Really Useful software listings.";
+my $siteHomepageDesc = "Lightweight software catalogue.";
 my $debug = 1;
 my $verbose = 0;
 
@@ -38,7 +41,9 @@ my $entryId;        # Entry-id variable of current working entry, used for reads
 my $tplTop;         # The 'top' half of the per-category template
 my $tplBottom;      # The 'bottom' half of the same. Entries will go between
 my $tplEntry;       # The blank HTML for each entry
+my $tplCatBar;
 my %catsFilledEntries;      # Hash of filled entries in HTML for each category
+my $writtenOut = 0; # A count of written out files.
 
 # List of known keys for each entry
 my @knownKeys = qw(title version category interface img_desc os_support order date_added desc);
@@ -70,7 +75,7 @@ sub read_partition_template {
     # Skip everything that isn't for the "easel" area
     while (<TPL>) {last if /\s*(<!--END RSRU-->)/; }
 
-    while (<TPL>) {$tplBottom .= $_; }
+    while (<TPL>) { $tplBottom .= $_; }
 
     say "TPLBOTTOM: $tplBottom" if ($debug);
     close TPL;
@@ -80,9 +85,14 @@ sub read_partition_template {
 # with the appropriate areas for our data marked out
 sub read_entrytpl {
     open(my $fh, "$blankentry") or die ("Fatal: couldn't open entry template $blankentry!");
-    while (<$fh>) { $tplEntry .= $_ ;}
+    while (<$fh>) { $tplEntry .= $_; }
     say "Entry Template:\n$tplEntry" if ($debug);
-    close TPL;
+    close $fh;
+
+    open($fh, "$blankCatBar") or die("Fatal: couldn't open catbar template $blankCatBar!");
+    while(<$fh>) { $tplCatBar .= $_; }
+    say "Catbar Template: $tplCatBar" if ($debug);
+    close $fh;
 }
 
 # Takes a key and prints the HTML for its contents
@@ -106,23 +116,51 @@ sub copy_res {
 # TODO: CSS
 }
 
+# Insert the cat links and title/desc
+sub prep_tpltop {
+    my $filledCats;
+    my $tempCat;
+    my $catFn;
+
+    $tplTop =~ s/{% HEAD_TITLE %}/$siteName/;
+    $tplTop =~ s/{% HEAD_DESC %}/$siteHeaderDesc/;
+
+    foreach my $cat (@cats) {
+        $catFn = $fnPre."_".$cat.".html";
+        $tempCat = $tplCatBar;
+        $tempCat =~ s/{% CAT_NAME %}/$cat/;
+        $tempCat =~ s/{% CAT_URL %}/$catFn/;
+        # How we make sure only the active cat page is seen as the current tab.
+        # Delete this for every cat except active, sub for "active" on cat
+        # Do in paint_template? 
+        $tempCat =~ s/IS_ACTIVE/IS_ACTIVE $cat/;
+        $filledCats .= $tempCat;
+    }
+
+    $tplTop =~  s/{% RSRU_CATS %}/$filledCats/;
+}
+
 # Print gathered entries into our template files. Do one for each cat.
 # Incomplete. TODO: complete, need to do for each cat...
 # ARGUMENTS: Cat name
 sub paint_template {
     my $catName = $_[0];
-    my $outFn = "$out/". $fnPre . $catName . ".html";
+    my $outFn = "$out/". $fnPre . "_" . $catName . ".html";
     my $currentEntry;
+    my $pageNo = 1;
 
     open (my $fh, '>', $outFn);
+    
+    $tplTop =~ s/{% RSRU_TITLE %}/$siteName :: $catName (Page $pageNo)/;
     print $fh $tplTop;
-
+    
     while (my ($entryId, $hashRef) = each (%entryKvs)) {
         $currentEntry = entrykvs_to_html $entryId;
         print $fh $$currentEntry;
     }
 
     print $fh $tplBottom; 
+    $writtenOut++;
     close $fh;
 }
 
@@ -184,7 +222,8 @@ read_entrytpl;
 say "<== Read Finished <==";
 
 say "<== Begin template interpolation... ==>";
+prep_tpltop;
 paint_template "test";
-say "<== Template interpolation finished ==>";
+say "<== Template interpolation finished. Wrote $writtenOut files. ==>";
 
 say "RSRU complete.";
