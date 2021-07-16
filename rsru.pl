239dd81f568eba46ce93aca9aabf5b90dcc0ff35
lordfeck <63065906+lordfeck@users.noreply.github.com>
2021-07-16 17:05:10 +0100

    [RSRU #7] Added homepage Also list of 5 most recent additions on homepage Also switched to use generic function to read in any file contents.

--- rsru.pl
+++ rsru.pl
@@ -25,12 +25,15 @@ my $out = "./rsru";
 my $tpl = "$tplinc/rsru_template.html";
 my $blankEntry = "$tplinc/rsru_entry.html";
 my $blankCatEntry = "$tplinc/rsru_cat.html";
+my $blankTplHp = "$tplinc/index.html";      # Blank HTML for the homepage
+my $blankTplHpEntry = "$tplinc/rsru_hp_entry.html";
 my $fnPre = "rsru";
 my $siteName = "RSRU";
 my $siteHeaderDesc = "Really Small, Really Useful software listings.";
-my $siteHomepageDesc = "Lightweight software catalogue.";
-my $debug = 1;
-my $verbose = 1;
+my $siteHomepageHeader = 'Welcome to RSRU!';
+my $siteHomepageDesc = "How do you do? Please enjoy your time browsing our lightweight software catalogue.";
+my $debug = 0;
+my $verbose = 0;
 my $clearDest = 1;
 
 # Default cats are always generated, even if empty.
@@ -45,18 +48,35 @@ my $entryId;        # Entry-id variable of current working entry, used for reads
 my $tplTop;         # The 'top' half of the per-category template
 my $tplBottom;      # The 'bottom' half of the same. Entries will go between
 my $tplEntry;       # The blank HTML for each entry
-my $tplCatBar;      # Blank HTML for each category
-my $cwTplTop;       # Current working template top (global scope)
+my $tplCatTab;      # Blank HTML for each category
+my $tplHp;          # Blank HTML for homepage
+my $tplHpEntry;     # Blank HTML for entries on the homepage
 my %catsFilledEntries;      # Hash of filled entries in HTML for each category
 my $writtenOut = 0; # A count of written out files.
 
 # Consts
 my $DATE_FORMAT = "%Y-%m-%d";
+my $MAX_CATS = 8;
+my $MIN_ENTRIES = 5;
+my $MAX_ENTRIES = 5;
 
 # List of known keys for each entry
 my @knownKeys = qw(title version category interface img_desc os_support order date desc dl_url is_highlight);
 
-sub sort_cat;
+# Declare Functions (but don't define yet)
+sub sort_entries;
+sub sort_all_entries;
+
+# Useful Helper Function.
+# ARGUMENTS: Filename to be read in. RETURNS: Contents of file as string.
+sub read_whole_file {
+    my $fileName = $_[0];
+    my $fileContent;
+    open(my $fh, "$fileName") or die ("Fatal: couldn't open $fileName!");
+    $fileContent .= $_ while <$fh>;
+    close $fh;
+    return $fileContent;
+}
 
 # Dump everything we've gathered into our KVS to stdout, format nicely. Intended for verbose mode.
 sub dump_kvs {
@@ -64,7 +84,7 @@ sub dump_kvs {
     print (keys %entryKvs, "\n");
 
     while (my ($entryId, $hashRef) = each (%entryKvs)) {
-        say "EID: $entryId";
+        say "EID: $entryId" if $debug;
         while (my ($key, $val) = each (%$hashRef)) {
             say "KEY: $key. VAL: $val";
         }
@@ -80,33 +100,12 @@ sub read_partition_template {
         $tplTop .= $_;
     }
 
-    say "TPLTOP: $tplTop" if ($debug);
-
     # Skip everything that isn't for the "easel" area
     while (<TPL>) { last if /\s*(<!--END RSRU-->)/; }
 
     # Then read the rest
     while (<TPL>) { $tplBottom .= $_; }
-
-    say "TPLBOTTOM: $tplBottom" if ($debug);
     close TPL;
-}
-
-# Now load in our entry template file. This should be a HTML table
-# with the appropriate areas for our data marked out
-sub read_entrytpl {
-    open(my $fh, "$blankEntry") or die ("Fatal: couldn't open entry template $blankEntry!");
-    while (<$fh>) { $tplEntry .= $_; }
-    say "Entry Template:\n$tplEntry" if ($debug);
-    close $fh;
-}
-
-# Load in the HTML for each category in the catbar
-sub read_cat_entrytpl {
-    open(my $fh, "$blankCatEntry") or die("Fatal: couldn't open catbar template $blankCatEntry!");
-    while(<$fh>) { $tplCatBar .= $_; }
-    say "Catbar Template: $tplCatBar" if ($debug);
-    close $fh;
 }
 
 # Takes a key and prints the HTML for its contents
@@ -153,18 +152,26 @@ sub paint_desc {
     $tplTop =~ s/{% HEAD_DESC %}/$siteHeaderDesc/;
 }
 
-# Insert the cat links. called by paint_template when it is used to process
-# each category page.
-# ARGUMENTS: active cat
-sub prep_tpltop {
+sub generate_cat_tabs {
     my $activeCat = $_[0];
-    my $filledCats; # Catbar
-    my $cwCat;  # Current Working Category
     my $catFn; # Cat Filename (used for hyperlinks)
+    my $cwCat;  # Current Working Category
+    my $filledCats; # Tabs of categories, eventually filled
 
+    # handle special case of index.html
+    $catFn = 'index.html';
+    $cwCat = $tplCatTab;
+    $cwCat =~ s/{% CAT_NAME %}/'home'/;
+    $cwCat =~ s/{% CAT_URL %}/$catFn/;
+    if ($activeCat eq 'index'){
+        $cwCat =~ s/{% IS_ACTIVE %}/active/;
+    }
+    $filledCats .= $cwCat;
+
+    # Now fill in the category tabs
     foreach my $cat (@cats) {
         $catFn = $fnPre."_".$cat.".html";
-        $cwCat = $tplCatBar;
+        $cwCat = $tplCatTab;
         $cwCat =~ s/{% CAT_NAME %}/$cat/;
         $cwCat =~ s/{% CAT_URL %}/$catFn/;
         # Set only the active cat to have the HTML class "active"
@@ -175,8 +182,21 @@ sub prep_tpltop {
         }
         $filledCats .= $cwCat;
     }
+    return $filledCats;
+}
 
-    $cwTplTop =~  s/{% RSRU_CATS %}/$filledCats/;
+# Insert the cat links. called by paint_template when it is used to process
+# each category page.
+# ARGUMENTS: active cat, page number
+sub prep_tpltop {
+    my ($activeCat, $pageNo) = @_; # FIXME pageNo isn't set for some reason
+    my $pageTxt = "";
+    $pageTxt = "(Page $pageNo)" if $pageNo;
+    my $catTabs = generate_cat_tabs($activeCat);
+    my $cwTplTop = $tplTop;
+    $cwTplTop =~ s/{% RSRU_TITLE %}/$siteName :: $activeCat $pageTxt/;
+    $cwTplTop =~  s/{% RSRU_CATS %}/$catTabs/;
+    return $cwTplTop;
 }
 
 # Print gathered entries into our template files. Do one for each cat.
@@ -186,16 +206,12 @@ sub paint_template {
     my $outFn = "$out/". $fnPre . "_" . $catName . ".html";
     my $currentEntry;
     my $pageNo = 1;
+    my $cwTplTop = prep_tpltop($catName, $pageNo); 
 
     open (my $fh, '>', $outFn);
-    
-    $cwTplTop = $tplTop;
-    $cwTplTop =~ s/{% RSRU_TITLE %}/$siteName :: $catName (Page $pageNo)/;
-    prep_tpltop $catName; 
-
     print $fh $cwTplTop;
     
-    for my $entryId (sort_cat $catName) {
+    for my $entryId (sort_entries $catName) {
         next unless ($entryKvs{$entryId}{"category"} eq $catName);
         $currentEntry = entrykvs_to_html $entryId;
         print $fh $$currentEntry;
@@ -206,22 +222,88 @@ sub paint_template {
     close $fh;
 }
 
+# Use the $tplHpEntry template to generate a list of entries for the homepage.
+# ARGUMENTS: An array of entry key names.
+sub generate_entries_hp {
+    my $hpEntries = "";
+    my ($cwHpEntry, $catFn, $cat);
+
+    for my $entry (@_) {
+       $cwHpEntry = $tplHpEntry;
+        
+       $cwHpEntry =~ s/{% ENTRY_NAME %}/$entryKvs{$entry}{"title"}/;
+       $cwHpEntry =~ s/{% ENTRY_DESC %}/$entryKvs{$entry}{"title"}/;
+       $cat = $entryKvs{$entry}{"category"};
+       $cwHpEntry =~ s/{% ENTRY_CAT %}/$cat/;
+       $catFn = $fnPre."_".$cat.".html";
+       $cwHpEntry =~ s/{% ENTRY_CAT_URL %}/$catFn/;
+       $hpEntries .= $cwHpEntry;
+    }
+    return $hpEntries;
+}
+
+# Print the homepage. ARGS: None.
+sub paint_homepage {
+    my $outFn = "$out/index.html";
+    
+    my $cwTplTop = prep_tpltop('index'); 
+    my @entries;
+
+    open (my $fh, '>', $outFn) or die ("Fatal: Couldn't open $outFn for writing!");
+    print $fh $cwTplTop;
+        
+    $tplHp =~ s/{% RSRU_HPHD %}/$siteHomepageHeader/;
+    $tplHp =~ s/{% RSRU_HPDESC %}/$siteHomepageDesc/;
+
+    print $fh $tplHp;
+
+    if (scalar (@entries) < $MIN_ENTRIES){
+        @entries = sort_all_entries($MAX_ENTRIES); 
+        print $fh generate_entries_hp(@entries);
+    } else {
+        say "Total entries are below $MIN_ENTRIES. Skipping latest on homepage.";
+    }
+
+    print $fh $tplBottom; 
+    $writtenOut++;
+    close $fh;
+}
+
+
 # Sort the given cat's entries. TODO sort order: DATE > RANK > ENTRY_NAME, currently only DATE.
-# Argument: Cat name
+# Argument: Cat name. [If not supplied, sort ALL entries by date]
 # Returns: A list that consists of ordered entry IDs for each cat
-sub sort_cat {
+sub sort_entries {
     my $cwCat = $_[0];
     my %entryDate;
 
     for my $entryId (keys %entryKvs) {
         if ($entryKvs{$entryId}{"category"} eq $cwCat) {
            $entryDate{$entryId} = $entryKvs{$entryId}{"date"};
-        }
+        } 
     }
 
     my @sorted = sort { $entryDate{$b} <=> $entryDate{$a} } keys %entryDate;
 
-    say "SORTED: @sorted, Length: ". length @sorted . " " if $debug;
+    say "SORTED: @sorted, Length: ". scalar @sorted . " " if $debug;
+    return @sorted;
+}
+
+# Do the same as above but with no heed to each category. Just returns a list
+# of all entries sorted by date.
+# ARGUMENTS: max index
+sub sort_all_entries {
+    my $max = $_[0];
+    my $itr = 0;
+    my %entryDate;
+    for my $entry (keys %entryKvs) {
+        break if ($itr == $max);
+        say "entry $entry and $entryKvs{$entry}{'date'}" if $debug;
+        $entryDate{$entry} = $entryKvs{$entry}{"date"};
+        $itr++;
+    }
+    my @sorted = sort { $entryDate{$b} <=> $entryDate{$a} } keys %entryDate;
+    say "Sorted are @sorted." if $debug;
     return @sorted;
 }
 
@@ -247,7 +329,6 @@ sub read_entry {
                 $entryData{"date"} = Time::Piece->strptime($val, $DATE_FORMAT); 
                 next;
             } elsif ($key eq "category"){
-                say 'CATEGORY FOUND';
                 unless ( first { /$val/ } @cats ){
                     say "New category found: $val." if $verbose;
                     push (@cats, $val);
@@ -295,12 +376,21 @@ mkdir $out unless -d $out;
 say "==> Begin read of $entrydir contents ==>";
 read_entrydir;
 say "CATS: @cats" if ($debug);
+say "Warning: More than $MAX_CATS exist in file. Template may be malformed." if (scalar (@cats) > $MAX_CATS);
 say "<== Read Finished <==";
 
 say "==> Begin read of template files ==>";
 read_partition_template;
-read_entrytpl;
-read_cat_entrytpl;
+
+# Now load in our entry template file. This should be a HTML table with the appropriate areas for our data marked out
+$tplEntry = read_whole_file($blankEntry);
+
+# Load in blank HTML for homepage items. Fills the global vars tplHp and tplHpEntry.
+$tplHp = read_whole_file($blankTplHp);
+$tplHpEntry = read_whole_file($blankTplHpEntry);
+
+# Load in the HTML for each category in the catbar
+$tplCatTab = read_whole_file($blankCatEntry);
 say "<== Read Finished <==";
 
 say "<== Begin template interpolation... ==>";
@@ -308,6 +398,7 @@ clear_dest if ($clearDest);
 copy_res;
 paint_desc;
 foreach my $cat (@cats) { paint_template $cat; }
+paint_homepage;
 say "<== Template interpolation finished. Wrote $writtenOut files. ==>";
 
 say "RSRU complete.";
